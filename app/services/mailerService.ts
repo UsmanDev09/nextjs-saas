603e1015b4f6d0d1fd185ccd07b4e844d6e4e8b6
Mukarram Ahmad <mukarram022@gmail.com>
2024-12-25 15:31:11 +0500

    mailer service added

--- app/services/mailerService.ts
+++ app/services/mailerService.ts
@@ -1,31 +1,59 @@
 import nodemailer from 'nodemailer';
 import { stringify } from 'qs';
+import { Resend } from 'resend';
 
+const resend = new Resend(process.env.RESEND_API_KEY as string);
 enum EmailTemplate {
   ForgotPassword = 'ForgotPassword',
   VerifyEmail = 'VerifyEmail',
 }
 
-const transporter = nodemailer.createTransport({
-  host: process.env.MAILHOG_HOST,
-  port: Number(process.env.MAILHOG_PORT),
-  secure: false,
-});
+// const transporter = nodemailer.createTransport({
+//   host: process.env.MAILHOG_HOST,
+//   port: Number(process.env.MAILHOG_PORT),
+//   secure: false,
+// });
 
 // Send email function
 const sendEmail = async (
   recipient: string,
   subject: string,
-  htmlContent: string
-): Promise<nodemailer.SentMessageInfo> => {
-  const mailOptions = {
-    from: `"Shaper" <${process.env.MAILER_EMAIL_FROM}>`,
-    to: recipient,
-    subject,
-    html: htmlContent,
-  };
+  htmlContent: string,
+): Promise<Response> => {
+  try {
+    const response = await resend.emails.send({
+      from: `Shipsaas <${process.env.DOMAIN_EMAIL}>`,
+      to: recipient,
+      subject,
+      html: htmlContent,
+    });
 
-  return transporter.sendMail(mailOptions);
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: 'Email sent successfully!',
+        details: response, // Include details about the sent email
+      }),
+      {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
+  } catch (error) {
+    console.error('Failed to send email:', error);
+
+    return new Response(
+      JSON.stringify({
+        success: false,
+        message: 'Failed to send email',
+        error: (error instanceof Error) ? error.message : 'Unknown error',
+      }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
+  }
 };
 
 // Get template title
@@ -41,7 +69,7 @@ const getTemplateTitle = (templateId: EmailTemplate): string => {
 const sendForgotPasswordEmail = async (
   recipient: string,
   token: string,
-  name?: string
+  name?: string,
 ): Promise<nodemailer.SentMessageInfo> => {
   const subject = getTemplateTitle(EmailTemplate.ForgotPassword);
   const resetLink = `${process.env.FRONTEND_HOST_URL}/reset-password?${stringify({ token })}`;
@@ -54,7 +82,7 @@ const sendForgotPasswordEmail = async (
 const sendVerificationEmail = async (
   recipient: string,
   code: string,
-  name: string|null
+  name: string|null,
 ): Promise<nodemailer.SentMessageInfo> => {
   const subject = getTemplateTitle(EmailTemplate.VerifyEmail);
   const htmlContent = `<p>Hi ${name},</p><p>Your verification code is: ${code}</p>`;
